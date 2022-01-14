a4e78ffa442a3759c7bccbbac25a76112ba69a1d
Jouca <progameur009@gmail.com>
2022-01-14 19:49:54 +0100

    Create diego.py

---
+++ modules/diego.py
@@ -0,0 +1,33 @@
+def clear_lines(tableau):
+    """
+    Permet de supprimer les lignes d'un tableau quand la ligne ne contient que
+    des 1, et de les décaler vers le bas et de garder le nombre de lignes
+    supprimé
+
+    >>> clear_lines([
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ])
+    [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ] 3
+    """
+    n = 0
+    copy_tableau = tableau.copy()
+    for i in range(len(tableau)):
+        if copy_tableau[i].count(1) == len(tableau[i]):
+            copy_tableau.pop(i)
+            copy_tableau.insert(0, [0] * len(tableau[0]))
+            n += 1
+    return copy_tableau,n
