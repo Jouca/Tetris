60d04b9795c9513e1eb47c5eafb72ee9750f563b
periergeia <88389522+periergeia@users.noreply.github.com>
2022-01-29 17:52:13 +0100

    fix import soucis répertoire
    @DominataurusRex j'ai split ta partie en deux, vu que la partie pygame gênait au bon fonctionnement du code principal (créer une fenêtre pygame supplémentaire n'est pas idéal d'autant plus qu'il te manque des fichiers, qui n'ont pas été ajouté, ...)
    Voici le chemin de ta partie règlement : ../test_menu/rules_paul.py
    je n'ai pas touché à ton code autrement (sauf le changement précédent de type pour le dictionnaire créé dans `border_dict` qui n'avait pas été fait et des correction de fautes d'orthographe :)).
    Pense à bien renseigner la variable `Liste1` dans ta docstring de la fonction  `list_conversion` quant au reste ça m'a l'air bien ^^
    ainsi que d'écrire une docstring à l'intérieur de la fonction pour `get_all_side` >^< 
    Il faudra juste que tu voies où est-ce qu'il y a un soucis avec les ghost pieces, tu as le temps, occupes-toi en priorité du menu des règles !
    
    Bon, finalement, je me suis permise de faire un test pylint et ait corrigé tous les défauts qu'il soulève, pas besoin de me remercier x) (pense seulement à le faire la prochaine fois).
    --> score de 9.82/10, je n'ai pas appliqué la suggestion d'utiliser enumerate au risque que le changement te perturbe, je te laisse le corriger toi-même pour un score parfait :)
    `************* Module paul`
    `modules\paul.py:77:4: C0200: Consider using enumerate instead of iterating with range and len (consider-using-enumerate)`

--- modules/paul.py
+++ modules/paul.py
@@ -1,3 +1,6 @@
+"""module codé par Paul TG8 (@DominataurusRex), contenant des fonctions utiles
+au bon fonctionnement du jeu Tetris."""
+
 def get_all_side(tetrimino_shape):
     """Renvoie un dictionnaire avec la liste des blocs aux
     extremités dans chaque orientation."""
@@ -29,30 +32,32 @@ def get_all_side(tetrimino_shape):
             east.append((i, column))
         if row != 10:
             south.append((row, i))
-    return {"0":north, "1":east, "2":south, "3":west}
+    return {0:north, 1:east, 2:south, 3:west}
 
 
 def list_conversion(liste, orientation):
-    """Prend une liste `Liste` et la renvoie sous forme "compactée"
+    """Prend une liste `liste` et la renvoie sous forme "compactée"
     par rapport à l'orientation.
     >>> list_conversion(Liste1, 0)
     [[(1, 0), 1], [(0, 1), 2]]
     """
-    # Rajoute une coordonnée bidon qui permet seulement de pouvoir parcourir toute la liste
-    # C'est 5 car la coordonnée maximal est 3
+    # Rajoute une coordonnée qui permet seulement de pouvoir parcourir
+    # toute la liste. C'est 5 car la coordonnée maximal est 3
     liste.append((5, 5))
     size = len(liste)
     rank = 1
     return_list = []
-    # Le nombre de coordonnée côte à côte, augmente quand il y en a plusieur et
-    # se réinitialise à chaque différence
+    # Le nombre de coordonnées côte à côte augmente quand il y en a
+    # plusieurs et se réinitialise à chaque différence
     dimension = 1
-    # Permet de savoir si cela doit être la coordonnée X ou Y qui doit être identique
+    # Permet de savoir si cela doit être la coordonnée X ou Y qui doit
+    # être identique
     if orientation % 2 == 0:
         spot = 0
     else:
         spot = 1
-    # Si il y a plusieur coordonnée côte à côte, cela garde la première coordonnée, donc la plus petite
+    # Si il y a plusieurs coordonnées côte à côte, cela garde la
+    # première coordonnée, donc la plus petite
     backup = liste[0]
     while rank < size:
         if backup[spot] != liste[rank][spot]:
@@ -69,45 +74,6 @@ def border_dict(tetrimino_shape):
     """Renvoie un dictionnaire des bloc à l'extremité de chaque côté."""
     all_side_dict = get_all_side(tetrimino_shape)
     return_dico = {}
-    for l in range(len(all_side_dict)):
-        return_dico[l] = list_conversion(all_side_dict[l], l)
+    for i in range(len(all_side_dict)):
+        return_dico[i] = list_conversion(all_side_dict[i], i)
     return return_dico
-
-
-# Règlement
-
-import pygame
-
-pygame.init()
-continuer = True
-screen = pygame.display.set_mode((1000, 500), pygame.RESIZABLE)
-pygame.display.set_caption("TETRIS")
-image = pygame.image.load("image.png").convert_alpha()
-pygame.display.set_icon(image)
-
-white = (255, 255, 255)
-green = (0, 255, 0)
-blue = (0, 0, 128)
-
-font = pygame.font.Font('freesansbold.ttf', 32)
-image2 = pygame.transform.scale(image, (1000, 500))
-
-
-while continuer:
-    width = screen.get_width()
-    height = screen.get_height()
-
-    text = font.render('Bonjour', True, (0, 59, 111))
-    textRect = text.get_rect()
-
-
-
-    textRect.center = (width // 2, height // 2)
-    screen.blit(text, textRect)
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            continuer = False
-    pygame.display.update()
-pygame.quit()
-
