307285e7a88ed93b84646997400758f5ab258fe5
BLASTUHQ <95956882+BLASTUHQ@users.noreply.github.com>
2022-01-29 09:10:34 +0100

    Update bastien.py

--- modules/bastien.py
+++ modules/bastien.py
@@ -1,1 +1,25 @@
-print("hello world")
+from telnetlib import LOGOUT
+import pygame
+
+pygame.init()
+
+continuer = True
+screen = pygame.display.set_mode((1000, 500), pygame.RESIZABLE)
+pygame.display.set_caption("TETRIS")
+image = pygame.image.load("prix.png").convert_alpha()
+pygame.display.set_icon(image)
+
+### import img ###
+logo = pygame.image.load('ressources/TETRIS.png').convert_alpha()
+### import img ###
+
+class Button:
+    pass
+
+def menu1():
+    screen.blit(logo, (100, 100))
+    pygame.display.flip()
+
+
+while continuer:
+    menu1()
