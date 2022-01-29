303aaac912a0c4b384bb546f9f18c74099980860
DominataurusRex <95956715+DominataurusRex@users.noreply.github.com>
2022-01-29 10:10:57 +0100

    Update paul.py

--- modules/paul.py
+++ modules/paul.py
@@ -72,3 +72,42 @@ def border_dict(tetrimino_shape):
     for l in range(len(all_side_dict)):
         return_dico[l] = list_conversion(all_side_dict[l], l)
     return return_dico
+
+
+# Règlement
+
+import pygame
+
+pygame.init()
+continuer = True
+screen = pygame.display.set_mode((1000, 500), pygame.RESIZABLE)
+pygame.display.set_caption("TETRIS")
+image = pygame.image.load("image.png").convert_alpha()
+pygame.display.set_icon(image)
+
+white = (255, 255, 255)
+green = (0, 255, 0)
+blue = (0, 0, 128)
+
+font = pygame.font.Font('freesansbold.ttf', 32)
+image2 = pygame.transform.scale(image, (1000, 500))
+
+
+while continuer:
+    width = screen.get_width()
+    height = screen.get_height()
+
+    text = font.render('Bonjour', True, (0, 59, 111))
+    textRect = text.get_rect()
+
+
+
+    textRect.center = (width // 2, height // 2)
+    screen.blit(text, textRect)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            continuer = False
+    pygame.display.update()
+pygame.quit()
+
