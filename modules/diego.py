2a3c76f871192f2b8f7c13dced1705536b03cc1c
periergeia <88389522+periergeia@users.noreply.github.com>
2022-02-10 20:48:13 +0100

    changement classe Button et pylint
    ajout de la transparence aux bouton et réussite du test pylint avec l'ajout de docstring, revue de la doctring

--- modules/diego.py
+++ modules/diego.py
@@ -3,14 +3,13 @@ fonctions utiles au bon fonctionnement du jeu Tetris."""
 
 import json
 import pygame
-
 from constant import COLOR
 from useful import get_font_size
 
+
 class Spritesheet:
-    """
-    Objet s'occupant d'un fichier type spritesheet.
-    """
+    """classe s'occupant d'un fichier type spritesheet."""
+
     def __init__(self, filename):
         self.filename = filename
         self.sprite_sheet = pygame.image.load(filename).convert()
@@ -45,13 +44,36 @@ class Spritesheet:
 
 
 class Button:
+    """crée un bouton visuel formaté avec le style général du jeu."""
 
-    def __init__(self, window, relative_position, text):
-        """
-        Exemple :
-        Button((200, 200, 100, 100), "Test", 60, (255, 255, 255))
-                 x    y    -    |
-        """
+    def __init__(self, window, relative_position, text, font_size=0):
+        """méthode constructeur de la classe :
+        - `window` est la fenêtre sur laquelle est créé le bouton ;
+        - `relative_position` correspond à un 4-uple (`x`, `y`, `w`, `h`)
+        indiquant la position et les dimensions relatives selon les dimensions
+        de la fenêtre, toutes les valeurs doivent être comprises entre 0 et 1
+        exclus afin que le bouton soit visible, dans l'ordre :
+            - position relative `x`, positionnement x par rapport à la largeur
+            de la fenêtre (sur bord gauche lorsque `x` vaut 0, droit lorsque
+            `x` vaut 1 (sort du cadre)) ;
+            - position relative `y`, positionnement y par rapport à la longueur
+            de la fenêtre (sur le bord haut lorsque `y` vaut 0, sur le bord bas
+            lorsque `y` vaut 1 auquel cas ne sera pas visible puisque le bouton
+            sortira du cadre de la fenêtre)) ;
+            - valeur `w`, représente la largeur du bouton selon la largeur de
+            la fenêtre (pour `w` égal à 0, le bouton est inexistant ce qui
+            n'est pas très intéressant, lorsque `w` vaut 1, le bouton possède
+            une largeur égale à celle de la fenêtre) ;
+            - valeur `h`, représente la longueur du bouton selon la longueur de
+            la fenêtre (pour `h` égal à 0, le bouton est inexistant ce qui
+            n'est pas très intéressant, lorsque `w` vaut 1, le bouton possède
+            une longueur égale à celle de la fenêtre)
+        - `text` est le texte associé au bouton, doit être une chaîne de
+        caractères ;
+        - font_size, un entier spécifiant la taille de la police pour le texte
+        à afficher sur le bouton visuel, dans le cas où elle n'est pas indiqué,
+        la taille dépendra de la hauteur du bouton.
+        >>> button = Button((0, 0, 1, 1), "Hello world !", 50)"""
         self.text = text
         window_w, window_h = window.get_size()
         x_value = round(relative_position[0] * window_w)
@@ -59,72 +81,56 @@ class Button:
         w_value = round(relative_position[2] * window_w)
         h_value = round(relative_position[3] * window_h)
         self.rect = pygame.Rect(x_value, y_value, w_value, h_value)
-        self.size = get_font_size(round(self.rect.h * 0.6))
-        self.font = pygame.font.SysFont("./others/Anton-Regular.ttf",
-                                        self.size)
-        self.text_image = self.font.render(self.text, 1 , COLOR['WHITE'])
+        # si la taille de la font n'est pas définie
+        if not font_size:
+            font_size = get_font_size(round(self.rect.h * 0.6))
+        font = pygame.font.SysFont("./others/Anton-Regular.ttf", font_size)
+        self.text_image = font.render(self.text, 1 , COLOR['WHITE'])
 
-    '''def __init__(self, rect, text, size, color):
-        """
-        Exemple :
-        Button((200, 200, 100, 100), "Test", 60, (255, 255, 255))
-                 x    y    -    |
-        """
-        self.text = text
-        self.size = size
-        self.font = pygame.font.SysFont(
-            "./others/Anton-Regular.ttf",
-            self.size,
-            bold = False
-        )
-        self.rect = pygame.Rect(rect)
-        self.text_image = self.font.render(self.text, 1 , color)'''
-    def resize(self, window):
-        ...
-
-    def draw(self, screen):
-        """
-        Permet de dessiner le bouton
-        """
-        pygame.draw.rect(screen, (150, 150, 150), self.rect, 4)
-        screen.blit(self.text_image, (self.text_image.get_rect(center = self.rect.center)))
+    def draw(self, surface):
+        """permet de dessiner le bouton sur une surface `surface` devant
+        être un objet pygame.Surface"""
+        # ##voir pour transparence
+        button_surface = pygame.Surface((self.rect.w, self.rect.h))
+        button_surface.set_alpha(175)
+        surface.blit(button_surface, (self.rect.x, self.rect.y))
+        pygame.draw.rect(surface, (150, 150, 150), self.rect, 4)
+        center_pos = self.text_image.get_rect(center = self.rect.center)
+        surface.blit(self.text_image, center_pos)
 
     def is_pressed(self, event):
-        """
-        Permet de détecter si le joueur a fait un clique gauche
-        sur le bouton.
-        """
+        """permet de détecter si le joueur a fait un clic gauche
+        sur le bouton. Renvoie un booléen, True si le bouton est cliqué,
+        False sinon."""
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
                 if self.rect.collidepoint(event.pos):
                     return True
         return False
 
-def clear_lines(content):
-    """
-    Permet de supprimer les lignes d'un tableau `content` quand la ligne
-    ne contient une ligne remplies (valeur autre que 0), et de les décaler
-    vers le bas et de garder le nombre de lignes supprimées
 
-    >>> clear_lines([
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    ])
-    [
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    ] 3
-    """
+def clear_lines(content):
+    """supprime les lignes remplies (valeur autre que 0) d'un
+    tableau `content`, les décale vers le bas, le tableau `content`
+    conserve le format initial, même nombre de lignes et de colonnes.
+    La fonction renvoie ce nouveau tableau ainsi que le nombre de
+    lignes supprimées.
+    >>> content = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+    >>> clear_lines(content)
+    [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]],
+     3"""
     nb_line_clear = 0
     copy_content = content.copy()
     for i in range(len(content)):
