ac815278a0ee4003a310bbc4469770bad024e22e
periergeia <88389522+periergeia@users.noreply.github.com>
2022-02-11 20:39:29 +0100

    module, et non plus fichier d'essai :)
    réussite de l'implémentation des menus dans le main avec quelques modifications.
    
    /!\ score pylint abominable (2.03/10 ;-;), à améliorer prochainement...

--- modules/bastien.py
+++ modules/bastien.py
@@ -1,72 +1,22 @@
-"""module codé par Bastien (@BLASTQ) TG8, contenant des fonctions pour
+"""module codé par Bastien (@BLASTUHQ) TG8, contenant des fonctions pour
 les menus du jeu Tetris. Repassage du code par Solène, (@periergeia)."""
 
+
 import pygame
-import sys
-from diego import Button
-from useful import get_font_size
-from constant import COLOR
-
-pygame.init()
-
-
-# CODE DANS main.py
-#########################################################
-FULLSCREEN_WIDTH = pygame.display.get_desktop_sizes()[0][1]
-WINDOW_HEIGHT = round(FULLSCREEN_WIDTH * 2/3)
-WINDOW_WIDTH = round(WINDOW_HEIGHT * 1.8)
-WINDOW_SIZE = (WINDOW_WIDTH, WINDOW_HEIGHT)
-
-
-# définition de la fenêtre pygame de taille dynamique
-tetris_window = pygame.display.set_mode(WINDOW_SIZE, pygame.RESIZABLE)
-window_data = {'size' : WINDOW_SIZE,
-               'width': WINDOW_WIDTH,
-               'height': WINDOW_HEIGHT,
-               'margin': round(0.05 * WINDOW_HEIGHT)}
-
-icon = pygame.image.load("./image/window_logo.png").convert_alpha()
-menubutton = pygame.image.load("./image/menubutton.png").convert_alpha()
-
-pygame.display.set_caption("TETRIS")
-pygame.display.set_icon(icon)
-
-
-################################################################
-
-
-
-### import img ###
-logo = pygame.image.load('./image/logo.jpg').convert_alpha()
-menu_background = pygame.image.load('./image/menu_background2.png').convert_alpha()
-### import img ###
-
-### import font ###
-myfont = pygame.font.SysFont("./others/Anton-Regular.ttf", 30)
-### import font ###
-
-
-def loop_starter_pack(event):
-    # appui sur la croix de la fenêtre
-    if event.type == pygame.QUIT:
-        # fermeture de la fenêtre
-        pygame.quit()
-        sys.exit()
-    
-    # dans le cas où l'utilisateur change la taille de la fenêtre
-    if event.type == pygame.VIDEORESIZE:
-        width, height = event.size
-        if width < 545:
-            width = 545
-        if width / height < 1.1:
-            width = round(1.8 * height)
-        if height < 303:
-            height = 303
-        window_size = (width, height)
-        tetris_window = pygame.display.set_mode(window_size, pygame.RESIZABLE)
+try:
+    from constant import COLOR
+    from diego import Button
+    from solene import gameplay
+    from useful import get_font_size, loop_starter_pack
+except ModuleNotFoundError:
+    from modules.constant import COLOR
+    from modules.diego import Button
+    from modules.solene import gameplay
+    from modules.useful import get_font_size, loop_starter_pack
 
 
 def create_main_menu(window):
+    logo = pygame.image.load('./image/logo.jpg').convert_alpha()
     logo_height = window.get_height() // 4
     logo_size = (round(340 * logo_height / 153), logo_height)
     logo_to_display = pygame.transform.scale(logo, logo_size)
@@ -105,7 +55,7 @@ def main_menu(window):
     proceed = True
     while proceed:
         for event in pygame.event.get():
-            loop_starter_pack(event)
+            loop_starter_pack(window, event)
             if event.type == pygame.VIDEORESIZE:
                 play_button, ranking_button, help_button = create_main_menu(window)
             if play_button.is_pressed(event):
@@ -123,11 +73,12 @@ def main_menu(window):
 
 
 def create_game_choice_menu(window):
+    menu_background = pygame.image.load('./image/menu_background2.png').convert_alpha()
     font_height = round(0.15 * window.get_height())
     font_size = get_font_size(font_height)
     font = pygame.font.SysFont("./others/Anton-Regular.ttf", font_size)
     statement = font.render('SELECTIONNEZ  UN  MODE', 1 , COLOR['WHITE'])
-    menu_background_to_display = pygame.transform.scale(menu_background, window.get_size())
+    background = pygame.transform.scale(menu_background, window.get_size())
     mode_a_button = Button(window,
                             (0.175,
                              0.4,
@@ -142,7 +93,7 @@ def create_game_choice_menu(window):
                          "MODE  B", font_size)
 
     frame = pygame.Surface(window.get_size())
-    frame.blit(menu_background_to_display, (0, 0))
+    frame.blit(background, (0, 0))
     frame.blit(statement, ((window.get_width() - statement.get_width()) // 2, round(0.175 * window.get_height())))
     mode_a_button.draw(frame)
     mode_b_button.draw(frame)
@@ -156,15 +107,15 @@ def game_choice_menu(window):
     proceed = True
     while proceed:
         for event in pygame.event.get():
-            loop_starter_pack(event)
+            window = loop_starter_pack(window, event)
             if event.type == pygame.VIDEORESIZE:
                 mode_a_button, mode_b_button = create_game_choice_menu(window)
             if mode_a_button.is_pressed(event):
-                game_choice_menu(window)
+                gameplay(window, (1, 0), lang)
                 proceed = False
                 return
             elif mode_b_button.is_pressed(event):
-                menuplay()
+                gameplay(window, (4, 4), lang) # ## level 4 hight 4
                 proceed = False
                 return
             '''elif back_button.is_pressed(event):
@@ -210,9 +161,3 @@ lang = 'EN'
 # provisoire, sans les menus
 level = 1
 mode_B = False
-
-
-if __name__ == "__main__":
-    main_menu(tetris_window)
-
-
