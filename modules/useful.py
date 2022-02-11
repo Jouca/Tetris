009bbc7b4b01cd24212515799a87a5afe1bd3c3e
periergeia <88389522+periergeia@users.noreply.github.com>
2022-02-11 20:30:55 +0100

    ajout fonction
    - fix soucis d'import
    - ajout de la fonction `loop_starter_pack` qui a besoin d'être utilisé dans plusieurs fichiers sans risquer le conflit d'import circulaire
    
    j'ajouterais les docstring un jour prochain, peut-être bien le jour où je ferais la relecture générale du code ...

--- modules/useful.py
+++ modules/useful.py
@@ -1,4 +1,10 @@
-from constant import FONT_HEIGHT
+import pygame
+import sys
+try:
+    from constant import FONT_HEIGHT
+except ModuleNotFoundError:
+    from modules.constant import FONT_HEIGHT
+
 
 def get_font_size(font_height):
     if font_height < 19:
@@ -8,3 +14,24 @@ def get_font_size(font_height):
         while font_height > FONT_HEIGHT[i]:
             i += 1
         return i + 12
+
+
+def loop_starter_pack(tetris_window, event):
+    # appui sur la croix de la fenêtre
+    if event.type == pygame.QUIT:
+        # fermeture de la fenêtre
+        pygame.quit()
+        sys.exit()
+    
+    # dans le cas où l'utilisateur change la taille de la fenêtre
+    if event.type == pygame.VIDEORESIZE:
+        width, height = event.size
+        if width < 545:
+            width = 545
+        if width / height < 1.1:
+            width = round(1.8 * height)
+        if height < 303:
+            height = 303
+        window_size = (width, height)
+        tetris_window = pygame.display.set_mode(window_size, pygame.RESIZABLE)
+    return tetris_window
